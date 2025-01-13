53625a8640578e951b036c3e393dd02bfc5f650e
Filipa Correia <57192880+Pipalipa@users.noreply.github.com>
2025-01-13 12:08:25 +0100

    Update test_app.py

--- unit_test/test_app.py
+++ unit_test/test_app.py
@@ -12,7 +12,7 @@ def test_streamlit_app():
     
     # Configurer le WebDriver
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
